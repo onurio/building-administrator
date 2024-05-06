97a709a2929b38ee20b329b75d61cc7b4d092d76
Omri Nuri <omrinuri@gmail.com>
2024-05-06 16:01:52 -0500

    fixed email reminders

--- src/utils/dbRequests/emails.js
+++ src/utils/dbRequests/emails.js
@@ -1,23 +1,22 @@
-import { setDoc,collection } from "firebase/firestore";
-import { withErrorHandling,db,customAlert } from "./dbutils";
+import { setDoc, doc } from "firebase/firestore";
+import { withErrorHandling, db, customAlert } from "./dbutils";
 
 export const sendEmail = async (info) => {
-    await withErrorHandling(async () => {
-      await setDoc(collection(db, "reciept_email"), {
-        ...info,
-        date_time: new Date().toISOString(),
-      });
-      customAlert(true, "Emails processing");
+  await withErrorHandling(async () => {
+    await setDoc(doc(db, "reciept_email", new Date().toISOString()), {
+      ...info,
+      date_time: new Date().toISOString(),
     });
-  };
-  
-  export const createReminderEmail = async (emails) => {
-    await withErrorHandling(async () => {
-      await setDoc(collection(db, "reminder_email"), {
-        emails: emails,
-        date_time: new Date().toISOString(),
-      });
-      customAlert(true, "Emails processing");
+    customAlert(true, "Emails processing");
+  });
+};
+
+export const createReminderEmail = async (emails) => {
+  await withErrorHandling(async () => {
+    await setDoc(doc(db, "reminder_email", new Date().toISOString()), {
+      emails: emails,
+      date_time: new Date().toISOString(),
     });
-  };
-  
+    customAlert(true, "Emails processing");
+  });
+};
